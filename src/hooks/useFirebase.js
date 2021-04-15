2f3a39c313d71b17f4add64c0b608a448ce0f2e6
Ritesh Patil <riteshsp2000@gmail.com>
2021-04-15 18:37:21 +0530

    Setup Firebase for gatsby

---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react"
+import getFirebase from "../utils/firebase"
+
+export default function useFirebase() {
+  const [instance, setInstance] = useState(null)
+
+  useEffect(() => {
+    setInstance(getFirebase())
+  }, [])
+
+  return instance
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,25 +5,48 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
+import useFirebase from "../hooks/useFirebase"
+
+const IndexPage = () => {
+  const firebase = useFirebase()
+  const [data, setData] = React.useState()
+
+  React.useEffect(() => {
+    if (firebase) {
+      const fetchData = async () => {
+        const firestoreData = await firebase
+          .firestore()
+          .collection("trial")
+          .doc("trialDoc")
+          .get()
+
+        setData(firestoreData.data().message)
+      }
+
+      fetchData()
+    }
+  }, [firebase])
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>{data ? data : "Loading..."}</p>
+      <StaticImage
+        src="../images/gatsby-astronaut.png"
+        width={300}
+        quality={95}
+        formats={["AUTO", "WEBP", "AVIF"]}
+        alt="A Gatsby astronaut"
+        style={{ marginBottom: `1.45rem` }}
+      />
+      <p>
+        <Link to="/page-2/">Go to page 2</Link> <br />
+        <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      </p>
+    </Layout>
+  )
+}
 
 export default IndexPage
