5cfba4f6b1ed418dea5cadf957f5b234039c791f
Ritesh Patil <riteshsp2000@gmail.com>
2021-04-15 19:07:22 +0530

    Add eslint and prettier

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react"
+
+// Utils
 import getFirebase from "../utils/firebase"
 
 export default function useFirebase() {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,52 +1,52 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import * as React from 'react';
+import {Link} from 'gatsby';
+import {StaticImage} from 'gatsby-plugin-image';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
-import useFirebase from "../hooks/useFirebase"
+import useFirebase from '../hooks/useFirebase';
 
 const IndexPage = () => {
-  const firebase = useFirebase()
-  const [data, setData] = React.useState()
+  const firebase = useFirebase();
+  const [data, setData] = React.useState('Loading...');
 
   React.useEffect(() => {
     if (firebase) {
       const fetchData = async () => {
         const firestoreData = await firebase
           .firestore()
-          .collection("trial")
-          .doc("trialDoc")
-          .get()
+          .collection('trial')
+          .doc('trialDoc')
+          .get();
 
-        setData(firestoreData.data().message)
-      }
+        setData(firestoreData.data().message);
+      };
 
-      fetchData()
+      fetchData();
     }
-  }, [firebase])
+  }, [firebase]);
 
   return (
     <Layout>
       <SEO title="Home" />
       <h1>Hi people</h1>
       <p>Welcome to your new Gatsby site.</p>
-      <p>{data ? data : "Loading..."}</p>
+      <p>{data}</p>
       <StaticImage
         src="../images/gatsby-astronaut.png"
         width={300}
         quality={95}
-        formats={["AUTO", "WEBP", "AVIF"]}
+        formats={['AUTO', 'WEBP', 'AVIF']}
         alt="A Gatsby astronaut"
-        style={{ marginBottom: `1.45rem` }}
+        style={{marginBottom: `1.45rem`}}
       />
       <p>
         <Link to="/page-2/">Go to page 2</Link> <br />
-        <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+        <Link to="/using-typescript/">Go to &quotUsing TypeScript&quot</Link>
       </p>
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
